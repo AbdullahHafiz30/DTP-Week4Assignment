941e54fb155017776fbaca73a2c2fb397bc3daca
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-14 19:59:56 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Abdullah Hafiz on 14/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
